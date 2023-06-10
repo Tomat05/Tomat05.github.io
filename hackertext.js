8c5adf55a027f944ad8d8404cab663f1cd05cd8e
Tomat05 <64409983+Tomat05@users.noreply.github.com>
2023-06-10 22:46:31 +0100

    Oops

--- hackertext.js
+++ hackertext.js
@@ -1,1 +1,34 @@
-/home/tom/HardDrive/ElCode/tomat05.net/hackertext.js
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const title = document.getElementById("title");
+
+let interval = null;
+let elementTexts = {
+    "title": "Thomas Wetherill".split(""),
+};
+
+function doHackerText(element) {
+    let iteration = 0;
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+        element.innerText = element.innerText
+        .split("")
+        .map((letter, index) => {
+            if (index < iteration) {
+                return elementTexts[element.id][index];
+            }
+            return letters[Math.floor(Math.random() * 52)]
+        })
+        .join("");
+
+        if (iteration >= element.innerText.length) {
+            clearInterval(interval);
+        }
+
+        iteration += 1/3;
+    }, 30);
+}
+
+window.onload = () => {
+    doHackerText(title);
+}
