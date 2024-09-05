be0d0b877b5d52d9d3c511ea7f4720968513ebe7
Tomat05 <wetherill.t@outlook.com>
2024-09-05 16:39:55 +0100

    stupid colour thingy

---
+++ nametocolour/main.js
@@ -0,0 +1,28 @@
+let fName = document.getElementById("fName");
+let mName = document.getElementById("mName");
+let sName = document.getElementById("sName");
+
+generateColour();
+
+function displayColour(colour) {
+  document.body.style.background = `rgb(${colour[0]}, ${colour[1]}, ${colour[2]})`;
+}
+
+function strip(str) {
+  return str.replace(/[^a-z]/gi, '').toLowerCase();
+}
+
+function generateColour() {
+  let name = [strip(fName.value), strip(mName.value), strip(sName.value)];
+
+  let colour = [0, 0, 0];
+
+  for (let i = 0; i < name.length; i++) {
+    for (let j = 0; j < name[i].length; j++) {
+      colour[i] += (name[i].charCodeAt(j) - 96);
+    }
+    colour[i] = (colour[i] * 4) % 256
+  }
+
+  displayColour(colour)
+}
